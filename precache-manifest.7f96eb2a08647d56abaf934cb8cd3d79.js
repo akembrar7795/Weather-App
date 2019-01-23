7e6da1073199980177bf6a869bb9229d611e637f
Akemjot  Brar <akemjotbrar@Akems-MacBook-Pro.local>
2019-01-23 14:19:19 -0500

    Updates

---
+++ precache-manifest.7f96eb2a08647d56abaf934cb8cd3d79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "dc8944941f304a5ac29a",
+    "url": "/weather-app/static/js/main.dc894494.chunk.js"
+  },
+  {
+    "revision": "67bc67be8c9acae784da",
+    "url": "/weather-app/static/js/1.67bc67be.chunk.js"
+  },
+  {
+    "revision": "dc8944941f304a5ac29a",
+    "url": "/weather-app/static/css/main.a2f0283d.chunk.css"
+  },
+  {
+    "revision": "67bc67be8c9acae784da",
+    "url": "/weather-app/static/css/1.a513f8b3.chunk.css"
+  },
+  {
+    "revision": "4956d21ccea2a68f4b63a2bfcea1959c",
+    "url": "/weather-app/index.html"
+  }
+];
